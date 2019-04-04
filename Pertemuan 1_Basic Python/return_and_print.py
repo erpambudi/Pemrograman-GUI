b98db1fcc9746c8c3abc3a61ddbff7cb8fc0da57
erpambudi <43690534+erpambudi@users.noreply.github.com>
2019-04-04 15:45:23 +0700

    Add files via upload
    Ini merupakan tugas 1 Pemrograman GUI

---
+++ Pertemuan 1_Basic Python/return_and_print.py
@@ -0,0 +1,16 @@
+def luas_persegi(sisi):
+	luas = sisi*sisi
+	return luas
+	print("Luas Persegi : ",luas)
+
+def luas_segitiga(alas, tinggi):
+	luas = alas*tinggi/2
+	print("luas_segitiga : %d" %luas)
+
+def hitung_umur(umur):
+	total = 2019-umur
+	print("Umur Anda : ",total)
+
+luas_persegi(45)
+luas_segitiga(100,50)
+hitung_umur(1998)
