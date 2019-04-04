4510be61e4a77a6c0d5f3069c92585d8cc4594d9
erpambudi <erpambudi76@gmail.com>
2019-04-04 12:35:33 +0700

    Program sebuah Nilai

---
+++ Nilai/nilai.py
@@ -0,0 +1,12 @@
+user_input = int(input("masukkan nilai : "))
+if user_input >= 80:
+	print("A")
+elif 70 <= user_input < 80:
+	print("B")
+elif 60 <= user_input < 70:
+	print("C")
+elif 40 <= user_input < 60:
+	print("D")
+else:
+	user_input < 40
+	print("E")
